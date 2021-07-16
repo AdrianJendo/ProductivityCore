86a2217c571437bcdd20174d772ff957f3989a09
Adrian Jendo <ajendo@uwaterloo.ca>
2021-07-15 20:23:29 -0700

    Initial Commit

---
+++ ProductivityCore/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProductivityCore
+//
+//  Created by Adrian Jendo on 2021-07-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProductivityCore/ProductivityCoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductivityCoreApp.swift
+//  ProductivityCore
+//
+//  Created by Adrian Jendo on 2021-07-15.
+//
+
+import SwiftUI
+
+@main
+struct ProductivityCoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
