3d521b8d2b0095c367b05fc473bff000b698861e
Adrian Jendo <ajendo@uwaterloo.ca>
2021-07-22 20:55:59 -0700

    Abandoning idea #1 for formatting title as textfield and whatnot, this is the last commit with that code

--- ProductivityCore/Views/TodoListView.swift
+++ ProductivityCore/Views/TodoListView.swift
@@ -14,34 +14,52 @@ struct TodoListView: View {
     @State private var highlightIndex = -1 // Highlighted index when updating a field
     @State private var editMode: EditMode = .inactive // Current state of EditMode used to handle editing
     @State private var showNavBarTitle = false // true when we should show the top navbar
+    @State private var navTitle = "" // Binding for when editing todolist title
+    @State private var focusNavTitle = false // Boolean for determing whether or not to focus on navtitle text field
 
     var body: some View {
         List {
-            VStack (spacing:0) {
-                GeometryReader { reader -> AnyView in
-                    let yAxis = reader.frame(in: .global).maxY
-                    if yAxis < 90 && !showNavBarTitle ||  yAxis > 90 && showNavBarTitle {
-                        withAnimation{ showNavBarTitle.toggle() }
-                    }
+//            VStack (spacing:0) {
+//                GeometryReader { reader -> AnyView in
+//                    let yAxis = reader.frame(in: .global).maxY
+//                    if yAxis < 90 && !showNavBarTitle ||  yAxis > 90 && showNavBarTitle {
+//                        withAnimation{ showNavBarTitle.toggle() }
+//                    }
 
-                    return AnyView (
-                        HStack {
-                            Text(list.wrappedTitle)
-                                .font(.system(size: 35, weight:.bold))
-                            if editMode == .active {
-                                Button(action: {}) {
+                    let stringWidth = navTitle.widthOfString(usingFont: UIFont.systemFont(ofSize: 35, weight: .bold))
+                    let defaultText = "List Title"
+                    let minWidth = defaultText.widthOfString(usingFont: UIFont.systemFont(ofSize: 35, weight: .bold))
+                    let width = stringWidth < 320 ? stringWidth > minWidth ? stringWidth : minWidth : 320
+//                    return AnyView (
+            if editMode == .active {
+                HStack {
+//                            if editMode == .active {
+                                ZStack {
                                     Image(systemName: "square.and.pencil")
                                         .foregroundColor(.systemBlue)
                                         .font(.system(size: 25, weight: .bold))
                                         .padding(.top, 2)
+                                        .padding(.leading, width+15)
+                                    TextField(
+                                        defaultText,
+                                        text: $navTitle,
+                                        onCommit: {
+                                            self.list.title = navTitle
+                                        }
+                                    )
+                                    .font(.system(size: 35, weight:.bold))
+                                    .frame(width: width+40)
                                 }
-                            }
-                            Spacer()
+//                            }
+//                            else {
+//                                Text(list.wrappedTitle)
+//                                    .font(.system(size: 35, weight:.bold))
+//                            }
                         }
-                    )
-                    
+                        .padding(.vertical, -10)
+//                    )
                 }
-            }
+//            }
             ForEach(list.itemsArray) { item in
                 CocoaTextField("Todo Task", text: Binding<String>(get: {item.text ?? "<none>"}, set: {updateTodoItem(item, $0)}))
                     .isFirstResponder(list.itemsArray.firstIndex{ $0 == item } == highlightIndex)
@@ -51,7 +69,9 @@ struct TodoListView: View {
             .onDelete(perform: deleteTodoItems)
             .onMove(perform: moveTodoItem)
         }
-        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarTitle(list.wrappedTitle)
+//        .navigationBarTitleDisplayMode(.inline)
+//        .navigationTitle(list.wrappedTitle)
         .toolbar {
             ToolbarItem(placement: ToolbarItemPlacement.principal) {
                 Text(list.wrappedTitle)
@@ -69,7 +89,7 @@ struct TodoListView: View {
                 else {
                     EditButton()
                 }
-
+                
                 Button(action: {
                     addTodoItem()
                 }) {
@@ -80,11 +100,20 @@ struct TodoListView: View {
         }
         .environment(\.editMode, $editMode)
         .onAppear(perform: setHighlightIndex)
+        .onDisappear(perform: {
+            self.list.title = navTitle
+        })
         .overlay(
             Text("Add A New Item")
                 .font(.system(size: 32, weight: .thin))
                 .opacity(list.itemsArray.count > 0 ? 0 : 1)
         )
+        .onChange(of: editMode) { (value) in
+            if value == .inactive {
+                self.list.title = navTitle
+            }
+        }
+//        .padding(.vertical, editMode == .active ? 12 : 0)
     }
 
     // Add new item
@@ -148,6 +177,7 @@ struct TodoListView: View {
                 self.highlightIndex = lastEmptyIndex
             }
         }
+        self.navTitle = self.list.wrappedTitle
     }
 
     // Helper function just to make sure the order doesn't get messed up (not used in prod)
@@ -156,6 +186,14 @@ struct TodoListView: View {
             print(i.order)
         }
         print("----------")
+    }
+}
+
+extension String {
+   func widthOfString(usingFont font: UIFont) -> CGFloat {
+        let fontAttributes = [NSAttributedString.Key.font: font]
+        let size = self.size(withAttributes: fontAttributes)
+        return size.width
     }
 }
 
